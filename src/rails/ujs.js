1782240eabf004e688138eb350ad11afc350a70f
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-03-24 16:13:57 +0300

    Rails UJS module update

--- src/rails/ujs.js
+++ src/rails/ujs.js
@@ -13,23 +13,28 @@
     }
   };
   
+  // adds XHR events to the element
+  var add_xhr_events = function(element, options) {
+    return Object.merge({
+      onCreate:   function() { element.fire('ajax:loading',  this) },
+      onComplete: function() { element.fire('ajax:complete', this) },
+      onSuccess:  function() { element.fire('ajax:success',  this) },
+      onFailure:  function() { element.fire('ajax:failure',  this) }
+    }, options);
+  };
+  
   // processes link clicks
   var try_link_submit = function(event, link) {
-    if (user_cancels(event, link)) return;
-    
     var method = link.get('data-method'), remote = link.get('data-remote');
     
+    if (user_cancels(event, link)) return;
     if (method || remote) event.stop();
     
     if (remote)
-      Xhr.load(link.href, {
+      Xhr.load(link.href, add_xhr_events(link, {
         method:     method || 'get',
-        spinner:    link.get('data-spinner'),
-        onCreate:   function() { link.fire('ajax:loading',  this) },
-        onComplete: function() { link.fire('ajax:complete', this) },
-        onSuccess:  function() { link.fire('ajax:success',  this) },
-        onFailure:  function() { link.fire('ajax:failure',  this) }
-      });
+        spinner:    link.get('data-spinner')
+      }));
       
     else if (method) {
       var param = $$('meta[name=csrf-param]')[0],
@@ -47,7 +52,7 @@
   var try_form_submit = function(event, button) {
     if (!user_cancels(event, button) && $(button.form).has('data-remote')) {
       event.stop();
-      button.form.send(); // oh yeah baby!
+      button.form.send(add_xhr_events(button.form));
     }
   };
 
@@ -65,8 +70,6 @@
 
     keydown: function(event) {
       var target = event.target, form = target.form;
-      
-      // trying to catch form submit via Enter pressing in an input field
       if (form && target.tagName === 'INPUT' && event.keyCode == 13) {
         try_form_submit(event, target);
       }
