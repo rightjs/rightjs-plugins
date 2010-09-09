9c1829b67498d9221782ade0a42f1b865d5d48a8
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-09-09 16:43:41 +0400

    jQuerysh plugin

---
+++ dist/jquerysh/lint.js
@@ -0,0 +1,9 @@
+/**
+ * The jquerysh module js-lint check
+ *
+ * Copyright (C) 2010 Nikolay Nemshilov
+ */
+load('util/test/rightly_check.js');
+
+rightly_check('build/right-jquerysh-src.js', [
+]);
---
+++ src/jquerysh/__init__.js
@@ -0,0 +1,9 @@
+/**
+ * jquerysh initialization script
+ *
+ * Copyright (C) 2010 Nikolay Nemshilov
+ */
+
+include_module_files(
+  'jquerysh'
+);
