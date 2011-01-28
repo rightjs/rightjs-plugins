afabccda074101e6f5ac955017b84416c437f6d8
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2011-01-28 17:43:06 +0300

    Keys some more 'alt' modifier fixes

--- src/keys/element.js
+++ src/keys/element.js
@@ -1,7 +1,7 @@
 /**
  * Overloading the Element#on method to handle those fancy names
  *
- * Copyright (C) 2010 Nikolay Nemshilov
+ * Copyright (C) 2010-2011 Nikolay Nemshilov
  */
 [RightJS.Document, RightJS.Element, RightJS.Window].each('include', {
   on: function() {
@@ -12,8 +12,7 @@
       key = (key[key.length - 1] || '').toUpperCase();
 
       if (key in RightJS.Event.Keys || /^[A-Z0-9]$/.test(key)) {
-        var meta   = /(^|\+|\-| )(meta)(\+|\-| )/i.test(name),
-            alt    = /(^|\+|\-| )(alt)(\+|\-| )/i.test(name),
+        var meta   = /(^|\+|\-| )(meta|alt)(\+|\-| )/i.test(name),
             ctrl   = /(^|\+|\-| )(ctl|ctrl)(\+|\-| )/i.test(name),
             shift  = /(^|\+|\-| )(shift)(\+|\-| )/i.test(name),
             code   = RightJS.Event.Keys[key] || key.charCodeAt(0),
@@ -29,8 +28,7 @@
           var raw = event._;
           if (
             (raw.keyCode === code) &&
-            (!alt   || raw.altKey) &&
-            (!meta  || raw.metaKey) &&
+            (!meta  || raw.metaKey || raw.altKey) &&
             (!ctrl  || raw.ctrlKey) &&
             (!shift || raw.shiftKey)
           ) {
