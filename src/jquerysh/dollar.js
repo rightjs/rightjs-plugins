e1f128e6df33a9a8d9a26d0fe4f080288ffd80c0
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2011-02-20 17:59:24 +0300

    jQuerysh moving the collection methods into an extenrally accessible hash

--- src/jquerysh/dollar.js
+++ src/jquerysh/dollar.js
@@ -6,26 +6,17 @@
 var $ = function(something) {
   switch(typeof something) {
     case 'string':
-        var hash = something[0], id = something.substr(1);
+      var hash = something[0], id = something.substr(1);
 
-        if (hash === '#' && (/^[\w\-]+$/).test(id)) {
-          return rjs_$(id);
-        } else if (hash === '<') {
-          return $E('div', {html: something}).first();
-        } else {
-          var rule = RightJS(something);
-
-          return $ext($$(something), {
-            live: function(event, callback) {
-              rule.on(event, callback);
-              return this;
-            },
-            die: function(event, callback) {
-              rule.stopObserving(event, callback);
-              return this;
-            }
-          });
-        }
+      if (hash === '#' && (/^[\w\-]+$/).test(id)) {
+        return rjs_$(id);
+      } else if (hash === '<') {
+        return $E('div', {html: something}).first();
+      } else {
+        hash = $$(something);
+        hash.cssRule = RightJS(something);
+        return $ext(hash, RightJS.jQuerysh.collectionMethods);
+      }
 
     case 'function':
       return rjs_$(document).onReady(something);
--- src/jquerysh/jquerysh.js
+++ src/jquerysh/jquerysh.js
@@ -4,5 +4,18 @@
  * Copyright (C) 2011 Nikolay Nemshilov
  */
 RightJS.jQuerysh = {
-  version: '2.2.0'
+  version: '2.2.0',
+
+  // collection methods
+  collectionMethods: {
+    live: function(event, callback) {
+      this.cssRule.on(event, callback);
+      return this;
+    },
+
+    die: function(event, callback) {
+      this.cssRule.stopObserving(event, callback);
+      return this;
+    }
+  }
 };
