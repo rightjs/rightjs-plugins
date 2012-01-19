98fe4bce67855fce89d449b9d8b25c42587f9a71
Nikolay Nemshilov <nemshilov@gmail.com>
2012-01-19 13:30:11 +0400

    Updating the building headers

--- dist/dnd/header.js
+++ dist/dnd/header.js
@@ -2,5 +2,5 @@
  * Drag'n'Drop module v%{version}
  * http://rightjs.org/plugins/drag-n-drop
  *
- * Copyright (C) 2009-2011 Nikolay Nemshilov
+ * Copyright (C) 2009-2012 Nikolay Nemshilov
  */
--- dist/rails/header.js
+++ dist/rails/header.js
@@ -2,5 +2,5 @@
  * RubyOnRails Support Module v%{version}
  * http://rightjs.org/plugins/rails
  *
- * Copyright (C) 2009-2011 Nikolay Nemshilov
+ * Copyright (C) 2009-2012 Nikolay Nemshilov
  */
